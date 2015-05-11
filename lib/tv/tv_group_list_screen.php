60feb89a8c9d9158acaddf307be7ec9f94c29398
igores-ru <info@igores.ru>
2015-05-11 12:36:06 +0200

    Update tv_group_list_screen.php
    Добавил кнопку перехода в настройки.

--- lib/tv/tv_group_list_screen.php
+++ lib/tv/tv_group_list_screen.php
@@ -2,17 +2,21 @@
 ///////////////////////////////////////////////////////////////////////////
 
 require_once 'lib/abstract_preloaded_regular_screen.php';
-
+require_once 'lib/abstract_controls_screen.php';
 ///////////////////////////////////////////////////////////////////////////
 
-class TvGroupListScreen extends AbstractPreloadedRegularScreen
+class TvGroupListScreen extends AbstractPreloadedRegularScreen 
+implements UserInputHandler
 {
     const ID = 'tv_group_list';
 
     ///////////////////////////////////////////////////////////////////////
 
     protected $tv;
-
+	public function get_handler_id()
+	{
+		return self::ID;
+	}
     ///////////////////////////////////////////////////////////////////////
 
     public function __construct($tv, $folder_views)
@@ -20,16 +24,24 @@ class TvGroupListScreen extends AbstractPreloadedRegularScreen
         parent::__construct(self::ID, $folder_views);
 
         $this->tv = $tv;
+		
+		UserInputHandlerRegistry::get_instance()->register_handler($this);
     }
 
     ///////////////////////////////////////////////////////////////////////
 
     public function get_action_map(MediaURL $media_url, &$plugin_cookies)
     {
+		//////////////////////////////////igores////////////////////////////////////////////
+		$setup_view = UserInputHandlerRegistry::create_action($this, 'do_setup_menu');
+        $setup_view['caption'] = 'Настройки плагина';
+		//////////////////////////////////////////////////////////////////////////////
+		
         return array
         (
             GUI_EVENT_KEY_ENTER => ActionFactory::open_folder(),
             GUI_EVENT_KEY_PLAY  => ActionFactory::tv_play(),
+			GUI_EVENT_KEY_D_BLUE => $setup_view,
         );
     }
 
@@ -70,6 +82,17 @@ class TvGroupListScreen extends AbstractPreloadedRegularScreen
     {
         return $this->tv->get_archive($media_url);
     }
+	public function handle_user_input(&$user_input, &$plugin_cookies)
+	{
+        hd_print('TVGroupListScreen: handle_user_input:');
+        foreach ($user_input as $key => $value)
+            hd_print("  $key => $value");
+			
+		if($user_input->control_id == 'do_setup_menu')
+		{
+		return ActionFactory::open_folder('setup');
+		}
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////
